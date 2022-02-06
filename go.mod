60cb1c0d8bff68c36755314d7ba7ec483a8bb87b
Максим Донец <maks.donets.95@outlook.com>
2022-02-06 19:15:32 +0300

    Add windows suppurt

--- go.mod
+++ go.mod
@@ -1,12 +1,17 @@
-module test
+module macs
 
-go 1.15
+go 1.17
 
 require (
-	github.com/go-cmd/cmd v1.4.0
-	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/schollz/wifiscan v1.1.1
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.3.0
-	github.com/theojulienne/go-wireless v1.0.0
+)
+
+require (
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/schollz/logger v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
 )
--- main.go
+++ main.go
@@ -1,76 +1,122 @@
 package main
 
 import (
-	"log"
+	"bufio"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/schollz/wifiscan"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
-/* For parse flag */
-var wifiinterface string
+const (
+	ERR_FILE_CREATE   = 300
+	ERR_FILE_WRITE    = 301
+	ERR_GET_MACS      = 302
+	ERR_FLAGS_EXECUTE = 303
+	EXEC_ERR          = 304
+	OUT_ERR           = 305
+	CONN_ERR          = 306
+)
+
+var (
+	wifiinterface, address string
+	scaningtime            int
+)
 
 func main() {
 	parse_flags()
 }
 
-/* Capture errors */
+/*Cathing errors*/
 func errors(err error, code int) {
 	if err != nil {
-		log.Println(err)
+		logrus.Info(err)
 		os.Exit(code)
 	}
 }
 
-/* Creates new file even if it exists */
-func create(filename string) {
-	file, err := os.Create(filename)
-	errors(err, 12)
+/*Writes data into a file*/
+func write(filename string, data string) {
+	file, err := os.OpenFile(address+".txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+	errors(err, ERR_FILE_CREATE)
 	defer file.Close()
+	_, err = file.WriteString(data + "\n")
+	errors(err, ERR_FILE_WRITE)
 }
 
-/* Writting data in target file */
-func writeInFile(filename string, data string) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0777)
-	errors(err, 2)
-	defer file.Close()
-	_, err = file.WriteString(data)
-	errors(err, 3)
+/*Disconnect wifi, set encoding code in comand rompt for all be fine*/
+func execs() {
+	ex := exec.Command("chcp", "437")
+	err := ex.Run()
+	errors(err, EXEC_ERR)
+	ex = exec.Command("netsh", "wlan", "disconnect")
+	err = ex.Run()
+	errors(err, EXEC_ERR)
 }
 
-/* Makes scanning within a minute and write macs in macs.txt for analizing */
+/*All actions together*/
 func Macs(cmd *cobra.Command, arg []string) {
-	log.Println("Scanning...")
-	ticker := time.NewTicker(1 * time.Second)
-	create("macs.txt") //it necessary for rewrite file always
+	execs()
+	var count, times, counMacs int
+	var mac wifiscan.Wifi
+	ticker := time.NewTicker(15 * time.Second)
+	logrus.Info("Scanning start...")
 	go func() {
 		for _ = range ticker.C {
+			times += 1
 			macs, err := wifiscan.Scan(wifiinterface)
-			errors(err, 4)
-			for _, mac := range macs {
-				writeInFile("macs.txt", mac.SSID+"\n")
+			errors(err, ERR_GET_MACS)
+			for counMacs, mac = range macs {
+				write(address+".txt", mac.SSID)
+				count++
+				counMacs++
 			}
+			write(address+".txt", "\n")
+			logrus.Infof("Scanned %v times %s and found %v macs", times, wifiinterface, counMacs)
+			counMacs = 0
 		}
 	}()
-	time.Sleep(60 * time.Second)
+	time.Sleep(time.Duration(scaningtime) * time.Second)
 	ticker.Stop()
-	log.Println("Scanning finished, check macs.txt")
+	logrus.Infof("Scanning stopped. Wrote %v strings in %s.txt", count, address)
+	err := connect()
+	errors(err, CONN_ERR)
 }
 
-/* Get network interface name as flag and show help message if it's not exist */
+/*parses flags*/
 func parse_flags() {
 	rootCmd := &cobra.Command{
 		Use:     "macs",
 		Version: "1.0",
-		Example: "\tsudo macs -i wlp3s0",
-		Run:     Macs,
-	}
-
-	rootCmd.Flags().StringVarP(&wifiinterface, "iface", "i", "", "mark outgoing interface")
-	rootCmd.MarkFlagRequired("iface")
-
+		Example: `   macs.exe -a "Ленина 5" -t 60 -s HomeWifi_5g`,
+		Run:     Macs}
+	rootCmd.Flags().StringVarP(&wifiinterface, "iface", "i", "", "set outgoing interface")
+	rootCmd.Flags().StringVarP(&address, "addr", "a", "", "set address")
+	rootCmd.Flags().IntVarP(&scaningtime, "time", "t", 120, "set scaning seconds")
+	rootCmd.MarkFlagRequired("addr")
 	err := rootCmd.Execute()
-	errors(err, 5)
+	errors(err, ERR_FLAGS_EXECUTE)
+}
+
+/*Connects to wifi after scaning*/
+func connect() error {
+	getProfs := exec.Command("netsh", "wlan", "show", "profiles")
+	out, err := getProfs.Output()
+	errors(err, OUT_ERR)
+	scanner := bufio.NewScanner(strings.NewReader(string(out)))
+	var profileName []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "All User Profile") {
+			profileName = strings.Fields(line)
+			break
+		}
+	}
+	connect := exec.Command("netsh", "wlan", "connect", profileName[4])
+	err = connect.Run()
+	return err
 }
