38752ef898261114664ad7ddf83bb870236eb41a
Maksim Donets <maks.donets.95@outlook.com>
2022-02-07 13:38:30 +0300

    Add funcs for capturing errors to be able make separate something actions for separate errors

--- go.mod
+++ go.mod
@@ -1,12 +1,9 @@
-module test
+module macs
 
 go 1.15
 
 require (
-	github.com/go-cmd/cmd v1.4.0
-	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/schollz/wifiscan v1.1.1
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.3.0
-	github.com/theojulienne/go-wireless v1.0.0
 )
--- main.go
+++ main.go
@@ -1,62 +1,93 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/schollz/wifiscan"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
+const (
+	ERR_OPEN_FILE   = 1000
+	ERR_WRITE_FILE  = 1001
+	ERR_WIFI_SCAN   = 1002
+	ERR_PARSE_FLAGS = 1003
+	ERR_CHMOD       = 1004
+)
+
 /* For parse flag */
-var wifiinterface string
+var (
+	wifiinterface, address string
+	scanningtime, interval int
+)
 
-func main() {
-	parse_flags()
-}
-
-/* Capture errors */
-func errors(err error, code int) {
+func errors(err error, code int, fn func(err error, code int, args ...interface{})) {
 	if err != nil {
-		log.Println(err)
-		os.Exit(code)
+		fn(err, code)
 	}
 }
 
-/* Creates new file even if it exists */
-func create(filename string) {
-	file, err := os.Create(filename)
-	errors(err, 12)
-	defer file.Close()
+func err_open_file(err error, code int, args ...interface{}) {
+	logrus.Error("Something wrong while open file\n", err, args)
+	os.Exit(code)
+}
+
+func err_write_file(err error, code int, args ...interface{}) {
+	logrus.Error("Something wrong while write file\n", err, args)
+	os.Exit(code)
+}
+
+func err_wifi_scan(err error, code int, args ...interface{}) {
+	logrus.Error("Something wrong while scanning wifi\n.", err, args)
+}
+
+func err_parse_flags(err error, code int, args ...interface{}) {
+	logrus.Error("Something wrong while parsing flags\n", err, args)
+	os.Exit(code)
+}
+
+func err_chmod(err error, code int, args ...interface{}) {
+	logrus.Error("Sumthing wrong while chmod\n", err, code, args)
+	os.Exit(code)
 }
 
 /* Writting data in target file */
 func writeInFile(filename string, data string) {
-	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0777)
-	errors(err, 2)
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0777)
+	errors(err, ERR_OPEN_FILE, err_open_file)
 	defer file.Close()
 	_, err = file.WriteString(data)
-	errors(err, 3)
+	errors(err, ERR_WRITE_FILE, err_write_file)
 }
 
 /* Makes scanning within a minute and write macs in macs.txt for analizing */
 func Macs(cmd *cobra.Command, arg []string) {
-	log.Println("Scanning...")
-	ticker := time.NewTicker(1 * time.Second)
-	create("macs.txt") //it necessary for rewrite file always
+	logrus.Info("Scanning...")
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	var count, countmacs int
 	go func() {
-		for _ = range ticker.C {
+		for sec := range ticker.C {
+			countmacs = 0
 			macs, err := wifiscan.Scan(wifiinterface)
-			errors(err, 4)
+			errors(err, ERR_WIFI_SCAN, err_wifi_scan)
 			for _, mac := range macs {
-				writeInFile("macs.txt", mac.SSID+"\n")
+				writeInFile(address+".txt", mac.SSID+"\n")
+				count++
+				countmacs++
 			}
+			fmt.Printf("Scanned %v macs in %v:%v:%v\n", countmacs, sec.Hour(), sec.Minute(), sec.Second())
 		}
 	}()
-	time.Sleep(60 * time.Second)
+	time.Sleep(time.Duration(scanningtime) * time.Second)
 	ticker.Stop()
-	log.Println("Scanning finished, check macs.txt")
+	err := os.Chmod(address+".txt", 0777)
+	errors(err, ERR_CHMOD, err_chmod)
+	fmt.Printf("Wrote %v strings in %s.txt\n", count, address)
+	wifiinterface, address = "", ""
+	count, scanningtime, interval = 0, 0, 0
 }
 
 /* Get network interface name as flag and show help message if it's not exist */
@@ -64,13 +95,20 @@ func parse_flags() {
 	rootCmd := &cobra.Command{
 		Use:     "macs",
 		Version: "1.0",
-		Example: "\tsudo macs -i wlp3s0",
-		Run:     Macs,
+		Example: "This example will scan wifis within 120 seconds every 5 seconds and write scanned macs in Ленина_5-2.txt\n\n" +
+			"\t" + `sudo macs -a Ленина_5-2 -t 120 -s 5`,
+		Run: Macs,
 	}
 
-	rootCmd.Flags().StringVarP(&wifiinterface, "iface", "i", "", "mark outgoing interface")
-	rootCmd.MarkFlagRequired("iface")
-
+	rootCmd.Flags().StringVarP(&wifiinterface, "iface", "i", "", "set outgoing interface")
+	rootCmd.Flags().StringVarP(&address, "addr", "a", "", "set address")
+	rootCmd.Flags().IntVarP(&scanningtime, "time", "t", 30, "set scanning time")
+	rootCmd.Flags().IntVarP(&interval, "sleep", "s", 13, "set scanning interval")
+	rootCmd.MarkFlagRequired("addr")
 	err := rootCmd.Execute()
-	errors(err, 5)
+	errors(err, ERR_PARSE_FLAGS, err_parse_flags)
+}
+
+func main() {
+	parse_flags()
 }
